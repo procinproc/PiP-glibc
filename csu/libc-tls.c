5378fb8b3444b00bbadddfc8e0d24551bd06eca0
ahori@riken.jp <ahori@wallaby9.aics-sys.riken.jp>
2016-12-14 14:29:38 +0900

    Change some TLS related constants hoping to relax TLS limitations

--- csu/libc-tls.c
+++ csu/libc-tls.c
@@ -60,7 +60,11 @@ struct dtv_slotinfo_list *_dl_tls_dtv_slotinfo_list;
 size_t _dl_tls_static_nelem;
 /* Size of the static TLS block.  Giving this initialized value
    preallocates some surplus bytes in the static TLS area.  */
+#ifndef NO_PIP_WORKAROUND
+size_t _dl_tls_static_size = 4096;
+#else
 size_t _dl_tls_static_size = 2048;
+#endif
 /* Size actually allocated in the static TLS block.  */
 size_t _dl_tls_static_used;
 /* Alignment requirement of the static TLS block.  */
