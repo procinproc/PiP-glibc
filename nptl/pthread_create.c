052510f0ff61b39d0e848eb1713e12b7b2dfcf83
SODA Noriyuki <soda@sra.co.jp>
2017-01-18 13:30:22 +0900

    export pip_clone_mostly_pthread() and pip_clone_minimum() from hori-san

--- nptl/pthread_create.c
+++ nptl/pthread_create.c
@@ -571,6 +571,16 @@ __pthread_create_2_1 (pthread_t *newthread, const pthread_attr_t *attr,
 versioned_symbol (libpthread, __pthread_create_2_1, pthread_create, GLIBC_2_1);
 
 int
+pip_clone_minimum (
+     pthread_t *newthread,
+     int clone_flags,
+     void *(*start_routine) (void *),
+     void *arg)
+{
+  return ENOSYS;
+}
+
+int
 pip_clone_mostly_pthread (
      pthread_t *newthread,
      int clone_flags,
