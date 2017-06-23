45b80dded6aec3c6fa72399b29aeb67dcbd6b020
ahori@riken.jp <ahori@riken.jp>
2017-06-23 16:27:41 +0900

    1) Undo prev change (elf/Versions and elf/dl-debug.c) 2) Add load-lock-initialized flag

--- elf/dl-debug.c
+++ elf/dl-debug.c
@@ -43,9 +43,7 @@ struct r_debug _r_debug;
    _r_debug.r_ldbase.  Returns the address of _r_debug.  */
 
 struct r_debug *
-#ifdef NO_PIP_WORKAROUND
 internal_function
-#endif
 _dl_debug_initialize (ElfW(Addr) ldbase, Lmid_t ns)
 {
   struct r_debug *r;
