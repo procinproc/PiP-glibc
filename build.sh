5f5d57d6987840ec61c05f6eefe6746f32e8852c
SODA Noriyuki <soda@sra.co.jp>
2018-07-25 18:51:24 +0900

    make $CC and $CXX really modifiable by environment variables
    this is sequel to 23be3091a0127a6c18f6fd99aff04cd2eeadf7b6,
    and to make PiP-gcc compilable on Ubuntu 18.04

--- build.sh
+++ build.sh
@@ -66,7 +66,7 @@ set -x
 make clean
 make distclean
 #
-$1/configure --prefix=$2 CC=gcc CXX=g++ "CFLAGS=${CFLAGS} ${opt_mtune} -fasynchronous-unwind-tables -DNDEBUG -g -O3 -fno-asynchronous-unwind-tables" --enable-add-ons=${opt_add_ons} --with-headers=/usr/include --enable-kernel=2.6.32 --enable-bind-now --build=${opt_build} ${opt_multi_arch} --enable-obsolete-rpc ${opt_systemtap} --disable-profile --enable-nss-crypt
+$1/configure --prefix=$2 CC="${CC}" CXX="${CXX}" "CFLAGS=${CFLAGS} ${opt_mtune} -fasynchronous-unwind-tables -DNDEBUG -g -O3 -fno-asynchronous-unwind-tables" --enable-add-ons=${opt_add_ons} --with-headers=/usr/include --enable-kernel=2.6.32 --enable-bind-now --build=${opt_build} ${opt_multi_arch} --enable-obsolete-rpc ${opt_systemtap} --disable-profile --enable-nss-crypt
 #
 make -j ${BUILD_PARALLELISM} ${opt_mflags}
 make install ${opt_mflags}
