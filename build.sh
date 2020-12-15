9c2870586aafca2268549fafbcde3efc30b43040
SODA Noriyuki <soda@sra.co.jp>
2020-12-15 12:44:06 +0900

    repair build.sh -b and -i handling

--- build.sh
+++ build.sh
@@ -104,14 +104,18 @@ else
 	opt_distro=
 fi
 
-case "$1" in
--b)	do_install=false; shift;;
--i)	do_build=false; shift;;
---prefix=*)
-    	prefix=`expr "$1" : "--prefix=\(.*\)"`; shift;;
--*)	usage;;
-*)	prefix=$1;;
-esac
+while	case "$1" in
+	-b)	do_install=false; true;;
+	-i)	do_build=false; true;;
+	--prefix=*)
+		prefix=`expr "$1" : "--prefix=\(.*\)"`; true;;
+	-*)	usage;;
+	'')	false;;
+	*)	prefix=$1; true;;
+	esac
+do
+	shift
+done
 
 if [ x"$prefix" == x ]; then
     echo >&2 "Error: <PREFIX> must be specifgied"
