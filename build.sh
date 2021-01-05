f5abb67d3ed3bdb024030e382813637fafdca696
ahori@riken.jp <ahori@riken.jp>
2021-01-05 09:39:12 +0900

    make clean before retry

--- build.sh
+++ build.sh
@@ -181,8 +181,8 @@ fi
 set -x
 
 if $do_build; then
-	make clean || true
-	make distclean || true
+	make clean
+	make distclean
 
 	$SRCDIR/configure --prefix=$prefix CC="${CC}" CXX="${CXX}" "CFLAGS=${CFLAGS} ${opt_mtune} -fasynchronous-unwind-tables -DNDEBUG -g -O3 -fno-asynchronous-unwind-tables" --enable-add-ons=${opt_add_ons} --with-headers=/usr/include --enable-kernel=2.6.32 --enable-bind-now --build=${opt_build} ${opt_multi_arch} --enable-obsolete-rpc ${enable_systemtap} --disable-profile ${enable_nss_crypt} ${opt_distro}
 
@@ -199,6 +199,7 @@ if $do_build; then
 	    fi
 	    cp $SRCDIR/intl/plural.c.OK $SRCDIR/intl/plural.c
 	    echo '===== try again ===='
+	    make clean
 	    make -j ${BUILD_PARALLELISM} ${opt_mflags}
             if [ $? != 0 ]; then
 		echo >&2 "PiP-glibc build error"
